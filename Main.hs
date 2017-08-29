a2859588d991bd08e7391c05e90ae91cc37b503f
CaioNunes <caiofelipe147@gmail.com>
2017-08-29 11:44:23 -0300

    Create class selection logic in start game, and files to any history

--- Main.hs
+++ Main.hs
@@ -7,12 +7,49 @@ import System.IO
 import Data.Char
 
 clear = system "clear"
+file_barb = "Map.txt"
+file_arc = "Map2.txt"
+file_hunt = "Map3.txt"
+play_again = "Deseja jogar novamente ? (1- SIM\n2-NAO\n)"
+goodbye = "Ate a proxima, aventureiro !"
 
-main = do
+menu :: IO()
+menu = do
+  putStrLn "===================================================";
+  putStrLn "Bem-Vindo Viajante, ao Diablo 3 - Haskell Version !\n Antes de começarmos a aventura, precisamos de uma informação importante:\n";
+  putStrLn "Com qual classe deseja jogar ?\n 1 - Bárbaro\n 2 - Arcanista\n 3 - Caçador de Demônios ";
+  option <- getLine;
+  case option of
+    "1" -> main_barb
+    "2" -> main_arc
+    "3" -> main_hunt
+
+main_barb = do
   clear
-  history <- fmap Text.lines (Text.readFile "Map.txt")
+  history <- fmap Text.lines (Text.readFile file_barb)
+
   let rpgMaps = buildTree history
   walk rpgMaps
-  putStrLn "Deseja jogar novamente ? (1- SIM\n2-NAO\n)"
+  putStrLn play_again
   playAgain <- readLn
-  if (playAgain == 1) then (main) else putStrLn ("Ate a proxima, aventureiro !")
+  if (playAgain == 1) then (menu) else putStrLn goodbye
+
+main_arc = do
+  clear
+  history <- fmap Text.lines (Text.readFile file_arc)
+
+  let rpgMaps = buildTree history
+  walk rpgMaps
+  putStrLn play_again
+  playAgain <- readLn
+  if (playAgain == 1) then (menu) else putStrLn goodbye
+
+main_hunt = do
+  clear
+  history <- fmap Text.lines (Text.readFile file_hunt)
+
+  let rpgMaps = buildTree history
+  walk rpgMaps
+  putStrLn play_again
+  playAgain <- readLn
+  if (playAgain == 1) then (menu) else putStrLn goodbye
